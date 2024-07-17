ab8e9b5fb5eecbe16fecebba73d75a04a3e9f19e
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-17 17:25:08 -0500

    Robot detector de objetos y que bota una luz

---
+++ exp2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+ev3 = EV3Brick()
+sensor = UltrasonicSensor(Port.S4)
+motorI = Motor(Port.B)
+motorD = Motor(Port.C)
+robot=DriveBase(motorI, motorD, wheel_diameter=56, axle_track=114)
+speed = 105
+Nspeed = -105
+while True:
+    ev3.light.on(Color.GREEN)
+    robot.drive(speed,0)
+    if sensor.distance()<=200:
+        robot.stop()
+        ev3.light.on(Color.RED)
+        ev3.speaker.beep()
+        wait(2000)
+        ev3.light.on(Color.YELLOW)
+        motorD.run(Nspeed)
+        motorI.run(Nspeed)
+        wait(2000)
+        motorD.stop()
+        motorI.run(Nspeed)
+        wait(2000)
